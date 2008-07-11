b2ef21f170fb7d13a389d0047f4894374a2ffc63
Michele Mattioni <mattions@gmail.com>
2008-07-11 16:38:50 +0100

    Set 10 iteration

--- MSNspine.py
+++ MSNspine.py
@@ -96,7 +96,7 @@ import steps.wmdirect as swmdirect
 ######
 # Wrapping the sim object in the number of iteration
 
-iterations = 1
+iterations = 10
 
 # Directory where to store the simulation
 currentDir = io.loader.createDir()
